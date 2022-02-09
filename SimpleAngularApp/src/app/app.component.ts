6daa5b3fe30bb6a0ada279d61984c378d0c4daba
Denis Popkov <pkvdenis@gmail.com>
2022-02-09 16:23:24 +0300

    SimpleAngularApp: access properties of child component using @ViewChild decorator.

--- SimpleAngularApp/src/app/app.component.ts
+++ SimpleAngularApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SimpleAngularApp';
+
+  @ViewChild('footer')
+  footerComponent: FooterComponent;
+
+  updateLastAccessed(): void {
+    console.log('Button "Update last accessed" was clicked');
+    console.log('The previous last accessed was', this.footerComponent.lastAccessed)
+    let lastAccessed = new Date().toString();
+    this.footerComponent.lastAccessed = lastAccessed;
+    console.log('lastAccessed updated to', lastAccessed)
+  }
 }
