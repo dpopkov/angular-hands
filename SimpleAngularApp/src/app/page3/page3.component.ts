cf1e88ce2a1596612b0f47c1674f225836acb85c
Denis Popkov <pkvdenis@gmail.com>
2022-02-12 18:37:45 +0300

    SimpleAngularApp: services: exercise 1 - delete last book (not production style).

--- SimpleAngularApp/src/app/page3/page3.component.ts
+++ SimpleAngularApp/src/app/page3/page3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from "../data.service";
 
 @Component({
   selector: 'app-page3',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page3Component implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+  }
+
+  removeLastBook(): void {
+    if (this.dataService.books.length > 0) {
+      const book = this.dataService.books.pop();
+      console.log('Removed book', book);
+    } else {
+      console.log('List of books is empty');
+    }
   }
 
 }
