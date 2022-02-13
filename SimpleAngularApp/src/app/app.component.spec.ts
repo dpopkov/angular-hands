9860090122c49d19a1cd9d1f1047c8ca7de9ebe8
Denis Popkov <pkvdenis@gmail.com>
2022-02-14 01:18:39 +0300

    SimpleAngularApp: testing: create test, fix AppComponent test.

--- SimpleAngularApp/src/app/app.component.spec.ts
+++ SimpleAngularApp/src/app/app.component.spec.ts
@@ -1,23 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {Page1Component} from "./page1/page1.component";
+import {Page2Component} from "./page2/page2.component";
+import {Page3Component} from "./page3/page3.component";
+import {FooterComponent} from "./footer/footer.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        Page1Component,
+        Page2Component,
+        Page3Component,
+        FooterComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'SimpleAngularApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.componentInstance;
     expect(app.title).toEqual('SimpleAngularApp');
   });
@@ -26,6 +38,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SimpleAngularApp app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Page 1');
   });
 });
--- SimpleAngularApp/src/app/data.service.spec.ts
+++ SimpleAngularApp/src/app/data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import {Book} from "./model/Book";
 
 describe('DataService', () => {
   let service: DataService;
@@ -12,5 +13,13 @@ describe('DataService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('addBook increases the size of the books array', () => {
+    const book = new Book();
+    const oldLength = service.books.length;
+    service.addBook(book);
+    // fail();
+    expect(service.books.length).toBeGreaterThan(oldLength);
   });
 });
