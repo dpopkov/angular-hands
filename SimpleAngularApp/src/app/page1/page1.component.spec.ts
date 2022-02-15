e7bc8a3740b32174614d82789de8c53c04db4bae
Denis Popkov <pkvdenis@gmail.com>
2022-02-15 15:23:42 +0300

    SimpleAngularApp: testing: testing components with service dependencies.

--- SimpleAngularApp/src/app/page1/page1.component.spec.ts
+++ SimpleAngularApp/src/app/page1/page1.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Page1Component } from './page1.component';
+import {Book} from "../model/Book";
+import {DataService} from "../data.service";
 
 describe('Page1Component', () => {
   let component: Page1Component;
@@ -22,4 +24,31 @@ describe('Page1Component', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('number of books is incremented correctly', () => {
+    const startValue = component.books.length;
+    const book = new Book();
+    book.author = 'Angular';
+    /*
+      The structure you need to use to reference a dependency
+      injected service in a test method:
+      const service = fixture.debugElement.injector.get(ClassOfService)
+    */
+    const dataService = fixture.debugElement.injector.get(DataService);
+    dataService.addBook(book);
+    const expectedUpdatedValue = startValue + 1;
+    expect(component.books.length).toEqual(expectedUpdatedValue);
+  });
+
+  it('number of books is incremented correctly (version 2)', () => {
+    const startValue = component.books.length;
+    const book = new Book();
+    book.author = 'Angular';
+    const dataService = new DataService();
+    component = new Page1Component(dataService);
+    component.ngOnInit();
+    dataService.addBook(book);
+    const expectedUpdatedValue = startValue + 1;
+    expect(component.books.length).toEqual(expectedUpdatedValue);
+  })
 });
