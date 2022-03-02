5d5243ee007daddd18d893297cba15c95772d879
Denis Popkov <pkvdenis@gmail.com>
2022-03-02 14:42:38 +0300

    BookingClient: set and read form control values for Room object.

--- BookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
+++ BookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
@@ -23,10 +23,19 @@ export class RoomEditComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.roomForm.patchValue(
+      {
+        roomName: this.room.name,
+        roomLocation: this.room.location
+      }
+    );
   }
 
   onSubmit() {
-    console.log('this.roomForm', this.roomForm);
+    this.room.name = this.roomForm.controls['roomName'].value;
+    this.room.location = this.roomForm.value['roomLocation']; // other way
+    console.log('updated room:', this.room);
+    // todo: call a method in the data service to save the room
   }
 
 }
