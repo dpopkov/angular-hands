a06c256484468fcc4f76ca0e5a459c9bd33885eb
Denis Popkov <pkvdenis@gmail.com>
2022-03-17 19:09:12 +0300

    booking: validating a JWT.

--- booking/src/main/java/learn/ang/booking/services/JwtService.java
+++ booking/src/main/java/learn/ang/booking/services/JwtService.java
@@ -2,6 +2,7 @@ package learn.ang.booking.services;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +11,7 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
@@ -37,5 +39,14 @@ public class JwtService {
                 .withClaim("role", role)
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME_MS))
                 .sign(Algorithm.RSA256(publicKey, privateKey));
+    }
+
+    /** Returns a JSON string containing the payload of the specified JWT. */
+    public String validateToken(String token) throws JWTVerificationException {
+        String encodedPayload = JWT.require(Algorithm.RSA256(publicKey, privateKey))
+                .build()
+                .verify(token)
+                .getPayload();
+        return new String(Base64.getDecoder().decode(encodedPayload));
     }
 }
