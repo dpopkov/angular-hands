a02e876db26b465af86d4951d23539d658fb7916
Denis Popkov <pkvdenis@gmail.com>
2022-03-17 16:50:24 +0300

    booking: prepare Spring Boot for JWT.

--- booking/src/main/java/learn/ang/booking/config/BasicAuthConfig.java
+++ booking/src/main/java/learn/ang/booking/config/BasicAuthConfig.java
@@ -15,15 +15,16 @@ public class BasicAuthConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("james")
-                .password("{noop}123")
-                .authorities("ROLE_ADMIN");
+                .withUser("james").password("{noop}123").authorities("ROLE_ADMIN")
+                .and()
+                .withUser("jane").password("{noop}123").authorities("ROLE_USER");
         // todo: this password should be encoded
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/api/basicAuth/**").permitAll()
                 .antMatchers("/api/basicAuth/**")
